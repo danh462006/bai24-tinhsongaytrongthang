c8d1762e84bfb59aa0d133a35e464b1b3efb8586
danh <nguyenthanhdanhpq1235@gmail.com>
2025-01-10 09:56:26 +0700

    them_bai _mang

--- bai24 tinhsongaytrongthang/bai24 tinhsongaytrongthang.cpp
+++ bai24 tinhsongaytrongthang/bai24 tinhsongaytrongthang.cpp
@@ -3,14 +3,30 @@
 
 #include <iostream>
 using namespace std;
-
-
 int main()
 {
-    int sothang ;
+    int sothang, sole[7] = { 1,3,5,7,8,10,12 }, sochan[4] = { 4,6,9,11 };
     cout << "tinh so ngay trong thang \n";
     cout << "nhap so thang "; cin >> sothang ;
- 
+    for (int i = 0; i < sizeof(sole) / sizeof(sochan[0]); i++)
+    {
+        if (sothang == sole[i])
+        {
+            cout << " thang " << sothang << " co 31 ngay ";
+        }
+    }
+    for (int i = 0; i < sizeof(sochan) / sizeof(sochan[0]); i++)
+    {
+        if (sothang == sochan[i])
+        {
+            cout << "thang " << sothang << "co 30 ngay ";
+        }
+    }
+if (sothang == 2 )
+{
+    cout << " thang 2 co 28 hoan 29 ngay ";
+}
+/*
     switch (sothang )
     {
     case 1 :
@@ -20,22 +36,21 @@ int main()
     case 8 :
     case 10 :
     case 12:
-                                    cout << "so ngay trong thang la : 31 ";
-                                    break;
+      cout << "so ngay trong thang la : 31 ";
+       break;
     case 2:
-                                        cout << " so ngay trong thang la : 28 hoac 29 ";
+         cout << " so ngay trong thang la : 28 hoac 29 ";
                                         break;
     case 4:
     case 6:
     case 9:
     case 11:
-                                                        cout << "so ngay trong thang la : 30 ";
-                                                        break;
-                                                    default:
-                                                        cout << " khong co thang " << sothang;
-
-                                                        break;
-    }
+        cout << "so ngay trong thang la : 30 ";
+          break;
+       default:
+     cout << " khong co thang " << sothang;
+      break;
+    }*/
 
     
 }
